552b6d25da899702e0e23c8705f90142577bcb7a
s1790986 <s.kyriaziorqirjazi-1@student.utwente.nl>
2016-10-04 19:24:46 +0200

    prototype of the website

---
+++ js/sofiak.js
@@ -0,0 +1,23 @@
+$(function () {
+    $('.tlt').textillate({
+    	loop:true,
+    	in: {
+    		effect: 'fadeIn',
+		    delayScale: 4.0,
+		    delay: 50,
+		    sync: false,
+		    shuffle: false,
+		    reverse: false,
+		    callback: function () {}
+    	},
+    	out: {
+    		effect: 'fadeOutRight',
+		    delayScale: 8.0,
+		    delay: 50,
+		    sync: false,
+		    shuffle: false,
+		    reverse: false,
+		    callback: function () {}
+    	}
+    });
+})
